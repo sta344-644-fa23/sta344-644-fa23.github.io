659be743dd0e6686ebf11aa617b55c2b3f85984b
Colin Rundel <rundel@gmail.com>
2023-10-24 22:38:36 -0400

    class

--- static/slides/Lec15_births_jags.R
+++ static/slides/Lec15_births_jags.R
@@ -1,18 +1,12 @@
 library(tidyverse)
+library(patchwork)
 
-births = read_csv("data/births_usa_1969.csv") |>
-  mutate(
-    has_leap_year = sum(month == 2 & day == 29),
-    .by = year
-  ) |> 
-  filter(!has_leap_year) |>
-  summarize(
-    births = mean(births),
-    .by = c(day_of_year)
-  ) |>
-  mutate(
-    scaled_log_births = c(scale(log(births)))
-  )
+ggplot2::theme_set(theme_minimal())
+
+
+
+
+
 
 model = "model{
   y ~ dmnorm(rep(0,N), inverse(Sigma))
@@ -32,18 +26,20 @@ model = "model{
   }  
 
   for(i in 1:3){
+    #sigma2[i] ~ dnorm(0, 1) T(0,)
+    #l[i] ~ dnorm(0, 1) T(0,)
     sigma2[i] ~ dt(0, 2.5, 1) T(0,)
-  }
-  for(i in 1:3)
     l[i] ~ dt(0, 2.5, 1) T(0,)
   }
-
-  alpha ~ dt(0, 2.5, 1) T(0,)
 }"
 
-dir.create("results/", showWarnings = FALSE, recursive = TRUE)
+dir.create("Lec15_results/", showWarnings = FALSE, recursive = TRUE)
 
-if (!file.exists("results/init_model.rds")) {
+flexiblas::flexiblas_load_backend("OPENBLAS-THREADS") |>
+  flexiblas::flexiblas_switch()
+#flexiblas::flexiblas_set_num_threads(24)
+
+if (!file.exists("Lec15_results/init_model.rds")) {
   m = rjags::jags.model(
     textConnection(model), 
     data = list(
@@ -53,9 +49,114 @@ if (!file.exists("results/init_model.rds")) {
       pi = pi,
       N = nrow(births)
     ),
-    n.adapt=1000
+    n.adapt=5000,
+    n.chains = 1
   )
-  saveRDS(m, file="results/init_model.rds")
+  saveRDS(m, file="Lec15_results/init_model.rds")
 } else {
-  m = readRDS("results/init_model.rds")
+  m = readRDS("Lec15_results/init_model.rds")
 }
+
+if (!file.exists("Lec15_results/fit_model.rds")) {
+  gp_coda = rjags::coda.samples(
+    m, variable.names=c("sigma2", "l"),
+    n.iter=5000,
+    thin=5
+  )
+  saveRDS(gp_coda, file="Lec15_results/fit_model.rds")
+} else {
+  gp_coda = readRDS("Lec15_results/fit_model.rds")
+}
+
+
+r = tidybayes::gather_draws(gp_coda, sigma2[i], l[i]) |>
+  ungroup() |>
+  mutate(
+    var = glue::glue("{.variable}[{i}]")
+  )
+
+ggplot(r, aes(x=.iteration, y=.value, color=as_factor(.chain))) +
+  geom_line() +
+  facet_wrap(~var, scales = "free_y") +
+  guides(fill = "none")
+
+ggplot(r, aes(x=.value, fill=as_factor(.chain))) +
+  geom_density() +
+  facet_wrap(~var, scales = "free") +
+  guides(fill = "none")
+
+cov_C1 = function(d, sigma2, l, per) {
+ sigma2[1] * exp(- (l[1] * d)^2)
+}
+
+cov_C2 = function(d, sigma2, l, per) {
+  sigma2[2] * exp(- (l[2] * d)^2 - 2 * (l[3] * sin(pi*d / per))^2)
+}
+
+cov_full = function(d, sigma2, l, per) {
+  cov_C1(d, sigma2, l, per) +
+  cov_C2(d, sigma2, l, per) +
+  ifelse(abs(d)<1e-6, sigma2[3] + 1e-6, 0)
+}
+
+
+
+y = births$scaled_log_births
+x = births$day_of_year / max(births$day_of_year)
+x_pred = runif(1000)
+
+post = tidybayes::gather_draws(gp_coda, sigma2[i], l[i]) |>
+  ungroup() |>
+  summarize(post_mean = mean(.value), .by=c(i,.variable)) |>
+  pivot_wider(id_cols = i, names_from = .variable, values_from = post_mean)
+
+full = dukestm::cond_predict(
+  y=y, x=x, x_pred=x_pred, cov_full, sigma2 = post$sigma2, l=post$l, per=7 / max(births$day_of_year)
+)
+
+comp_C1 = dukestm::cond_predict(
+  y=y, x=x, x_pred=x_pred, sigma2 = post$sigma2, l=post$l, per=7 / max(births$day_of_year),
+  cov_f_o  = cov_full,
+  cov_f_p  = cov_C1,
+  cov_f_po = cov_C1
+)
+
+comp_C2 = dukestm::cond_predict(
+  y=y, x=x, x_pred=x_pred, sigma2 = post$sigma2, l=post$l, per=7 / max(births$day_of_year),
+  cov_f_o  = cov_full,
+  cov_f_p  = cov_C2,
+  cov_f_po = cov_C2
+)
+
+pred = tibble(
+  x_pred,
+  day_of_year = x_pred * max(births$day_of_year),
+  y_hat = apply(full,1,mean),
+  Cov1 = apply(comp_C1, 1, mean),
+  Cov2 = apply(comp_C2, 1, mean)
+)
+
+
+( 
+  ggplot(pred, aes(x=day_of_year)) +
+    geom_line(aes(y=y_hat), color='blue', linewidth=1.1) +
+    geom_line(data=births, aes(y=scaled_log_births), alpha=0.3)
+) / (
+  ( 
+    ggplot(pred, aes(x=day_of_year)) +
+      geom_line(aes(y=Cov1), color='red') +
+      geom_line(data=births, aes(y=scaled_log_births), alpha=0.3) 
+  ) + (
+    ggplot(pred, aes(x=day_of_year)) +
+      geom_line(aes(y=Cov2), color='green') #+
+      #geom_line(data=births, aes(y=scaled_log_births), alpha=0.3) 
+  )
+)
+
+
+
+
+
+
+
+
